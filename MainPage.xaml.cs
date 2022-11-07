ef64e08a253d8f534754dc8b468b7b228d357892
Ian <ianmaciel76@gmail.com>
2022-11-07 17:54:54 -0300

    adicionando binding estatico

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,21 +1,28 @@
-﻿using PokedexMaui.Services;
+﻿using PokedexMaui.Models;
+using PokedexMaui.Services;
 
 namespace PokedexMaui;
 
 public partial class MainPage : ContentPage
 {
-    private readonly IPokeApiService _pokeApiService;
+    public static Pagination<Pokemon> MockPaginationPokemons { get; private set; }
+
+    static MainPage()
+    {
+        MockPaginationPokemons = new Pagination<Pokemon>()
+        {
+            Next = null,
+            Previus = null,
+            Results = new List<Pokemon>() {
+                    new Pokemon { Name = "test1", Url = "testurl1" },
+                    new Pokemon { Name = "test2", Url = "testurl2" },
+                }
+        };
+    }
 
     public MainPage(IPokeApiService pokeApiService)
     {
-        InitializeComponent();
-        _pokeApiService = pokeApiService;
-    }
-
-
-    private async void OnCounterClickedAsync(object sender, EventArgs e)
-    {
-        string str = await _pokeApiService.GetPokemons();
+        InitializeComponent();     
     }
 }
 
---
+++ Models/Pagination.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PokedexMaui.Models
+{
+    public class Pagination<T>
+    {
+        public string? Next { get; set; }
+        public string? Previus { get; set; }
+        public ICollection<T> Results { get; set; }
+        public int Count { get { return Results.Count; } }
+
+        public Pagination(string str)
+        {
+            var requestResult = deserialize(str);
+            this.Next = requestResult.Next;
+            this.Previus = requestResult.Previus;
+            this.Results = requestResult.Results;
+        }
+
+        public Pagination()
+        {
+            Next = null;
+            Previus = null;
+            Results = new List<T>();
+        }
+
+        private static Pagination<T> deserialize(string str)
+        {
+
+            var value = JsonSerializer.Deserialize<Pagination<T>>(
+                str,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+            return value;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
+
